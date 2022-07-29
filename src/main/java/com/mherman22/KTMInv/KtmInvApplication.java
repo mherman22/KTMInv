023be7a85664659854dfb22611c4ea61db08b8d7
Herman Muhereza <hermanmuhereza22@gmail.com>
2022-07-29 14:09:54 +0300

    initialising the backend for ktminv

---
+++ src/main/java/com/mherman22/KTMInv/KtmInvApplication.java
@@ -0,0 +1,13 @@
+package com.mherman22.KTMInv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtmInvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtmInvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mherman22/KTMInv/KtmInvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mherman22.KTMInv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtmInvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
