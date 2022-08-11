04e1c3511beb5f227aea4ddfc014f056aa3fe199
mherman22 <hermanmuhereza22@gmail.com>
2022-08-11 15:30:48 +0300

    more changes

--- src/main/java/com/mherman22/KTMInv/Repository/PaymentRepository.java
+++ src/main/java/com/mherman22/KTMInv/Repository/PaymentRepository.java
@@ -1,9 +1,10 @@
 package com.mherman22.KTMInv.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.mherman22.KTMInv.models.Payment;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long>{
 
 }
