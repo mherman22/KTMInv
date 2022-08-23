587975c19d68557d12cc673da41075932ddde027
mherman22 <hermanmuhereza22@gmail.com>
2022-08-23 14:30:15 +0300

    customer controller

--- src/main/java/com/mherman22/KTMInv/controllers/CustomerController.java
+++ src/main/java/com/mherman22/KTMInv/controllers/CustomerController.java
@@ -1,5 +1,52 @@
 package com.mherman22.KTMInv.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mherman22.KTMInv.Exceptions.customer.CustomerNotFoundException;
+import com.mherman22.KTMInv.Repository.CustomerRepository;
+import com.mherman22.KTMInv.models.Customer;
+
+@RestController
+@RequestMapping("/ktm/v1/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping("/list")
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Customer getById(@PathVariable Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException(id));
+    }
+
+    @PostMapping("/create")
+    public Customer createCustomer(@RequestBody Customer newCustomer) {
+        return customerRepository.save(newCustomer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        customerRepository.deleteById(id);
+    }
 
 }
